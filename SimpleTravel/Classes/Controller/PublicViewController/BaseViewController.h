798ee45967d245ac3302b609154da50045ebc6a2
DevFeng <fjkvscy@sina.com>
2017-03-29 17:25:36 +0800

    3.29
    add BaseViewController

---
+++ SimpleTravel/Classes/Controller/PublicViewController/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  SimpleTravel
+//
+//  Created by hzyx01 on 17/3/29.
+//  Copyright © 2017年 hzyx01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
